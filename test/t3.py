80aefbb90fab9dbfbe7b993013faa6a9ae356362
iamwrm <iamwrm@gmail.com>
2022-09-16 11:32:27 +0000

    bench python excutor

--- test/t3.py
+++ test/t3.py
@@ -3,23 +3,30 @@ import time
 
 
 def wait_on_a(a):
-    time.sleep(1)
-    a = a+1
+    for i in range(2000000):
+        a = a+1
     return a
 
 
-executor = ThreadPoolExecutor(max_workers=19)
+def excutor_run(max_workers):
+    executor = ThreadPoolExecutor(max_workers=max_workers)
 
-t1 = time.time()
+    t1 = time.time()
 
-promise_list = []
-for i in range(20):
-    p = executor.submit(wait_on_a, i)
-    promise_list.append(p)
+    promise_list = []
+    for i in range(10):
+        p = executor.submit(wait_on_a, i)
+        promise_list.append(p)
 
-result_list = []
-for i in promise_list:
-    result_list.append(i.result())
+    result_list = []
+    for i in promise_list:
+        result_list.append(i.result())
 
-print("result_list sum: ", sum(result_list))
-print(time.time() - t1)
+    print(f"{max_workers}")
+    print("result_list sum: ", sum(result_list))
+    print(time.time() - t1)
+
+
+excutor_run(max_workers=2)
+excutor_run(max_workers=4)
+excutor_run(max_workers=8)
