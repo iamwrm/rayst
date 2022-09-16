3d63bb162659da4134098c02602fbf84b954deff
iamwrm <iamwrm@gmail.com>
2022-09-16 07:32:05 +0000

    add multiply

--- rayst/src/lib.rs
+++ rayst/src/lib.rs
@@ -6,9 +6,15 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[pyfunction]
+fn multiply(a: isize, b: isize) -> PyResult<isize> {
+    Ok(a * b)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn rayst(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(multiply, m)?)?;
     Ok(())
 }
--- test/t1.py
+++ test/t1.py
@@ -1,3 +1,4 @@
 import rayst
 
 print(rayst.sum_as_string(5, 20))
+print(rayst.multiply(5, 20))
